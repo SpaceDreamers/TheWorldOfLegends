40e0db85c12866c4cf1a98e9f180b79b969bdbd7
YaroslavGladun <gladi41414@gmail.com>
2018-09-17 20:56:03 +0300

    Add NPC_Mob_Version1.0

---
+++ NPS_Mob_Version_1.0/NPC_Mob/Assets/_Scripts/MobClasification.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mobs
+{
+    abstract class Mob
+    {
+        public GameObject GameObjectMob = new GameObject(); // This object - mob body
+        //public Mob() { }
+    }
+    class SomeoneMob1 : Mob
+    {
+        
+    }
+}
---
+++ NPS_Mob_Version_1.0/NPC_Mob/Assets/_Scripts/MoveModel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace ContrallerMove
+{
+
+    public class MoveModel : MonoBehaviour
+    {
+
+
+        public Transform target; // Обєкт, за яким буде рухатися персонаж
+        public int distance;     // Дистанція до цілі, перейшовши яку персонаж зупиняється
+        NavMeshAgent navMesh;    // Штучний інтелект
+
+        private float timeToStop; // Завдяки цій змінній обєкт буде зупинятися не зразу як перейде дистанцію
+
+
+        void Start()
+        {
+            timeToStop = 1;
+            navMesh = GetComponent<NavMeshAgent>(); // Присвоюємо обєкту navMesh, NavMeshAgent обєкту, на якого кинутий скрипт
+        }
+
+        void Update()
+        {
+            // Чек дистанції для зупинки
+            if (Vector3.Distance(target.transform.position, transform.position) < distance)
+            {
+                if (timeToStop < 0)
+                {
+                    navMesh.GetComponent<NavMeshAgent>().enabled = false; // Вимикаємо AI
+                    gameObject.GetComponent<Animator>().Play("Stay");     // Включаємо анімацію Stay
+                }
+                else timeToStop -= Time.deltaTime;
+            }
+            else
+            {
+                navMesh.GetComponent<NavMeshAgent>().enabled = true;  // Вмикаэмо AI
+                gameObject.GetComponent<Animator>().Play("Walk");     // Включаємо анімацію Walk
+                navMesh.SetDestination(target.position);              // В якості цілі виберажмо позицію відповідного обєкту
+                timeToStop = 2;
+
+            }
+        }
+    }
+}
